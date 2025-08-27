f4707a8f39892208690f14ed0556c0b3c710e629
cmerhab <connormerhab@gmail.com>
2025-08-27 12:13:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+from pathlib import Path
+def get_book_text(filepath):
+    with open(filepath, "r", encoding="utf-8") as f: #opens file in read only mode and sets it as f
+        return f.read()                              #reads that f file
+
+def main():
+    filepath = Path("books/frankenstein.txt")
+    print(get_book_text(filepath))
