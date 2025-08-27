6d6d40bdecd737bed73dadd8fd6f85b5cdcede92
cmerhab <connormerhab@gmail.com>
2025-08-27 13:07:05 -0400

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from stats import get_book_count
+from stats import get_char_count
 
 def get_book_text(filepath): #function to return the file as a string
     with open(filepath, "r", encoding="utf-8") as file: #opens file in read only mode and sets it as f
@@ -10,5 +11,6 @@ def main():
     #print(get_book_text(filepath))
     word_count = get_book_count(filepath)
     print(f"{word_count} words found in the document")
-
+    char_count = get_char_count(filepath)
+    print(f"{char_count}")
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,22 @@ def get_book_count(filepath):
     except FileNotFoundError:
         print(f"Error: File '{filepath}' not found!")
         return 0
+    
+
+def get_char_count(filepath):
+    char_count = {} #empty dictionary 
+    try:
+        with open(filepath, 'r') as file:
+            content = file.read()
+            text = content.lower() # converts it all to lowercase
+
+        for char in text:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+    except FileNotFoundError:
+        print(f"File not found: {filepath}")
+    except Exception as e:
+        print(f"An error occured: {e}")
+    return char_count
