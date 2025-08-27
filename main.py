b87405728eaee0d4ddcf115707cb094498581c5a
cmerhab <connormerhab@gmail.com>
2025-08-27 14:12:48 -0400

    step 202022

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def get_book_text(filepath): #function to return the file as a string
 def main():
     filepath = Path("books/frankenstein.txt")
     #print(get_book_text(filepath))
+    filepath_string = str(filepath) #This is just books/frankentein.txt not the actual book text
     word_count = get_book_count(filepath)
-    print(f"{word_count} words found in the document")
-    #char_count = get_char_count(filepath)
-    #print(f"{char_count}")
     sort_dict = sort_count(filepath)
+    print("============ BOOKBOT ============\n")
+    print(f"Analyzing book found at {filepath_string}...\n")
+    print(f"Found {word_count} total words\n")
+    print(f"--------- Character Count -------\n")
     print(f"{sort_dict}")
 main()
